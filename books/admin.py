532ade1902a265ce35c998d1d6a6d3715fc69959
extremiumlar <nurullojonobloqulov@gmail.com>
2025-04-23 10:03:04 +0500

    modellar yozildi
    bu loyiha djangoni takroran o'tgan ekan shuning uchun bu loyihani hozircha stop

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book, BookAuthor, BookReview, Author
+
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'isbn']
+    list_display = ('title', 'isbn', 'description')
+
+admin.site.register(Book, BookAdmin)
+
+class BookAuthorAdmin(admin.ModelAdmin):
+    list_display = ('book', 'author')
+
+admin.site.register(BookAuthor, BookAuthorAdmin)
+
+admin.site.register(BookReview)
+admin.site.register(Author)
+
+
+
--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # o'zim yaratilishiga sabab bo'lgan applar
+    'books',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'goodreads2.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -74,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'goodreads',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': 'cheklanma',
+        'PORT': '5432',
     }
 }
 
